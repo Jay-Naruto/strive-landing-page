6613311902bfc513633f15f69b5ee9e3d342e30c
Jay-Naruto <jaynarutomistry@gmail.com>
2023-11-05 16:54:04 -0700

    old works again

--- components/Features/Features.js
+++ components/Features/Features.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import styles from "./Features.module.css"
+import { useMediaQuery } from 'react-responsive'
+import Typewriter from 'typewriter-effect';
 export default function Features() {
+    const isDesktopOrLaptop = useMediaQuery({
+        query: '(max-width: 1000px)'
+      })
+
+    
   return (
     <div id='features' className={styles.features}>
             <div className={styles.featuresBox}>
@@ -18,9 +25,27 @@ export default function Features() {
                     <div className={styles.border2}></div>
 
                     <div  className={styles.boxContent}>
-                    <div>
-                    Invest in creators using our creator launchpad
-                    </div>
+
+                        {
+                            isDesktopOrLaptop ?
+                            <div>
+                            <Typewriter
+                                    options={{
+                                        strings: ['Invest in creators using our creator launchpad'],
+                                        autoStart: true,
+                                        loop: true,
+                                        deleteSpeed:Infinity
+                                    }}
+                              
+                                    />
+                            </div>
+                            :
+                            <div>
+                            Invest in creators using our creator launchpad
+                            </div>
+        
+                        }
+
                     <div>
                     <img className={styles.f1} src='./f1.png' alt=''/>
 
@@ -35,9 +60,26 @@ export default function Features() {
                     <div className={styles.border2}></div>
                     <div className={styles.border}></div>
                     <div  className={styles.boxContent}>
-                    <div >
-                    Raise funds for your creative projects using our fundraising platform
-                    </div>
+
+                    {
+                            isDesktopOrLaptop ?
+                            <div>
+                            <Typewriter
+                                    options={{
+                                        strings: ['Raise funds for your creative projects using our fundraising platform'],
+                                        autoStart: true,
+                                        loop: true,
+                                        deleteSpeed:Infinity
+                                    }}
+                                    />
+                            </div>
+                            :
+                            <div >
+                            Raise funds for your creative projects using our fundraising platform
+                            </div>
+        
+                        }
+
                     <div>
                     <img className={styles.f2} src='./f2.png' alt=''/>
 
@@ -56,9 +98,30 @@ export default function Features() {
                     <div className={styles.border2}></div>
                     <div className={styles.border}></div>
                     <div  className={styles.boxContent}>
-                    <div className={styles.div1}>
-                    Engage with your fans / investors using messaging, streams, merchandise drops, subscriptions and anything else you can think of.
-                    </div>
+
+
+                    {
+                            isDesktopOrLaptop ?
+                            <div>
+                            <Typewriter
+                                    options={{
+                                        strings: [' Engage with your fans / investors using messaging, streams, merchandise drops, subscriptions and anything else you can think of.'],
+                                        autoStart: true,
+                                        loop: true,
+                                        deleteSpeed:Infinity
+                                    }}
+                                    />
+                            </div>
+                            :
+                            <div className={styles.div1}>
+                            Engage with your fans / investors using messaging, streams, merchandise drops, subscriptions and anything else you can think of.
+                            </div>
+        
+                        }
+
+
+
+
                     <div  className={styles.div2}>
                     <img className={styles.f3} src='./f3.png' alt=''/>
 
@@ -74,9 +137,28 @@ export default function Features() {
                     <div className={styles.border2}></div>
                     <div className={styles.border}></div>
                     <div  className={styles.boxContent}>
-                    <div  className={styles.div2}>
-                    Trade utility/social/fan tokens launched by top creators and earn from the growth of leading influencers and creators
-                    </div>
+
+                    {
+                            isDesktopOrLaptop ?
+                            <div>
+                            <Typewriter
+                                    options={{
+                                        strings: [' Trade utility/social/fan tokens launched by top creators and earn from the growth of leading influencers and creators'],
+                                        autoStart: true,
+                                        loop: true,
+                                        deleteSpeed:Infinity
+                                    }}
+                                    />
+                            </div>
+                            :
+                            <div  className={styles.div2}>
+                            Trade utility/social/fan tokens launched by top creators and earn from the growth of leading influencers and creators
+                            </div>
+        
+                        }
+
+
+
                     <div  className={styles.div1} >
                     <img  className={styles.f4} src='./f4.png' alt=''/>
 
