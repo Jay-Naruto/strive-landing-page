a30edeeccaa5cae4bea8d7e6b560c92618a17779
Jay-Naruto <jaynarutomistry@gmail.com>
2023-11-05 11:10:34 -0700

    btn remove

--- components/Creator/Creator.js
+++ components/Creator/Creator.js
@@ -108,14 +108,19 @@ export default function Creator() {
 
 
             </div>
-            <div  className={styles.parallaxBtn4}>
 
-            <img
-        
-            className={styles.creator_btn4} src='./creator-btn3.png' alt=''/>
+            {
+              !isDesktopOrLaptop &&
+              <div  className={styles.parallaxBtn4}>
 
+              <img
+          
+              className={styles.creator_btn4} src='./creator-btn3.png' alt=''/>
+  
+  
+              </div>
+            }
 
-            </div>
 
 
 
